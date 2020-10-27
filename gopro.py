74a0daaa8016ad0ba6d60619a330127ad985ed1e
fenggao <fegao@microsoft.com>
2020-10-27 14:59:23 -0700

    add music

--- gopro.py
+++ gopro.py
@@ -1,7 +1,6 @@
 import argparse
 import subprocess
 
-
 def parse(input_file):
     videoFiles = []
     times = []
@@ -12,7 +11,7 @@ def parse(input_file):
                 videoFiles.append(line.rstrip())
                 times.append([])
             else:
-                times[-1].append((items[0], items[1]))
+                times[-1].append(items)
 
     return videoFiles, times
 
@@ -20,19 +19,34 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser(description='gopro tool')
     args.add_argument('-i', '--input', type=str, dest='input', help="input file")
     args.add_argument('-o', '--output', type=str, dest='output', help="output file")
+    args.add_argument('--cutOnly', dest='cutOnly', action='store_true')
     args = args.parse_args()
     videos, times = parse(args.input)
     count = len(videos)
     clip = 0
+    clips = []
     for i in range(count):
         for t in times[i]:
             cutCmd = 'ffmpeg -i {} -ss {} -to {} -async 1 {}.mp4'.format(videos[i], t[0], t[1], clip + 1)
-            clip = clip + 1
             subprocess.run(cutCmd, stdout=subprocess.PIPE)
+            if len(t) == 3:
+                removeAudioCmd = 'ffmpeg -i {}.mp4 -c copy -an {}.an.mp4'.format(clip + 1, clip + 1)
+                subprocess.run(removeAudioCmd, stdout=subprocess.PIPE)
+
+                # addMusicCmd = 'ffmpeg -i {}.an.mp4 -i {} -shortest -map 0:0 -map 1:0 -c copy {}.music.mp4'.format(clip + 1, t[2], clip + 1)
+                # subprocess.run(addMusicCmd, stdout=subprocess.PIPE)
+
+                clips.append('{}.an.mp4'.format(clip + 1))
+            else:
+                clips.append('{}.mp4'.format(clip + 1))
+            clip = clip + 1
+
+    if args.cutOnly:
+        exit()
 
     # merge the results
     file = open('list.txt', 'w')
-    file.writelines(["file {}.mp4\n".format(i + 1) for i in range(clip)])
+    file.writelines(["file {}\n".format(clipName) for clipName in clips])
     file.close()
     concatCmd = 'ffmpeg -safe 0 -f concat -i list.txt -c copy {}'.format(args.output)
     subprocess.run(concatCmd, stdout=subprocess.PIPE)
